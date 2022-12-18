d111692a96ab00678dbba72f987cbce5e8902779
Neha Galla <nsg2057@nyu.edu>
2022-12-18 11:23:57 -0500

    Exception handler added

--- src/main/java/edu/nyu/nsg2057/webscraper/exception/ExceptionController.java
+++ src/main/java/edu/nyu/nsg2057/webscraper/exception/ExceptionController.java
@@ -5,10 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ValidationException;
+
 @ControllerAdvice
 public class ExceptionController {
+    @ExceptionHandler(value = ValidationException.class)
+    public ResponseEntity<Object> validationException(Exception exception) {
+        return new ResponseEntity<>("Invalid request " + exception.getLocalizedMessage() , HttpStatus.BAD_REQUEST);
+    }
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<Object> exception(Exception exception) {
         return new ResponseEntity<>("Something went wrong in Backend" + exception.getLocalizedMessage() , HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
 }
